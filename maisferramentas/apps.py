bdca4cf456574c616e2eb4281552f74d25557667
Makis Jeanty <markisj128@gmail.com>
2026-08-01 05:26:57 -0300

    fix: add missing __init__.py and apps.py to maisferramentas app

---
+++ maisferramentas/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MaisFerramentasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'maisferramentas'
+    verbose_name = 'Mais Ferramentas'
