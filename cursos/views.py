33343cf31c919e5e125be596cd60c1937fede192
Makis Jeanty <markisj128@gmail.com>
2026-08-05 16:26:09 -0300

    feat(cursos): melhora organizacao dos cursos com metricas de licoes/duracao e agrupamento por modulos

--- cursos/views.py
+++ cursos/views.py
@@ -1,12 +1,29 @@
+from django.db.models import Count, Sum, Prefetch
 from django.shortcuts import render, get_object_or_404
 from .models import Curso, Modulo, Licao
 
 
 def lista_cursos(request):
-    cursos = Curso.objects.filter(ativo=True)
+    cursos = Curso.objects.filter(ativo=True).prefetch_related('modulos__licoes').order_by('ordem')
+    
+    # Anexa estatísticas calculadas para cada curso
+    cursos_com_stats = []
+    for curso in cursos:
+        total_licoes = 0
+        duracao_total = 0
+        for modulo in curso.modulos.all():
+            licoes = modulo.licoes.all()
+            total_licoes += len(licoes)
+            duracao_total += sum(l.duracao_minutos for l in licoes)
+        
+        curso.total_licoes = total_licoes
+        curso.duracao_total = duracao_total
+        cursos_com_stats.append(curso)
+
     return render(request, 'cursos/lista.html', {
-        'cursos': cursos,
+        'cursos': cursos_com_stats,
     })
+
 
 
 def trilha_curso(request, slug):
